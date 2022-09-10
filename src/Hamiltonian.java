dc86fe75261627dc4522a80ec469c13d99c27064
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2022-09-10 19:57:48 +0530

    Updated

--- src/Hamiltonian.java
+++ src/Hamiltonian.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Hamiltonian {
     static int[][] graph;
-    static int[] soln;
+    static int[] solution;
     static int n, count = 0;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -17,11 +17,11 @@ class Hamiltonian {
         }
         scan.close();
         // Instantiate Solution array(1 Indexed), (Default Value is 0)
-        soln = new int[n + 1];
+        solution = new int[n + 1];
         System.out.println("Hamiltonian Cycle are");
         // In a cycle source vertex doesn't matter
         // Assign Starting Point to prevent repetitions
-        soln[1] = 1;
+        solution[1] = 1;
         // Call Hamiltonian function to start backtracking from vertex 2
         hamiltonian(2);
         if (count == 0) {
@@ -32,7 +32,7 @@ class Hamiltonian {
         while (true) {
             nextValue(k);
             // No next vertex so return
-            if (soln[k] == 0) {
+            if (solution[k] == 0) {
                 return;
             }
             // if cycle is complete then print it else find next vertex
@@ -46,24 +46,24 @@ class Hamiltonian {
     static void nextValue(int k) {
         // Finds next feasible value
         while (true) {                                      //Things to check in bounding function
-            soln[k] = (soln[k] + 1) % (n + 1);              //1. No duplicate
+            solution[k] = (solution[k] + 1) % (n + 1);              //1. No duplicate
             // If no next vertex is left, then return       //2. Any vertex taken should have an edge from previous vertex
-            if (soln[k] == 0) {                             //3. There should be an edge from last vertex to first vertex
+            if (solution[k] == 0) {                             //3. There should be an edge from last vertex to first vertex
                 return;
             }
             // If there exists an edge
-            if (graph[soln[k - 1]][soln[k]] != 0) {
+            if (graph[solution[k - 1]][solution[k]] != 0) {
                 int j;
                 // Check if the vertex is not repeated
                 for (j = 1; j < k; j++) {
-                    if (soln[j] == soln[k]) {
+                    if (solution[j] == solution[k]) {
                         break;
                     }
                 }
                 // If vertex is not repeated
                 if (j == k) {
                     // If the vertex is not the last vertex, or it completes the cycle then return
-                    if (k < n || (k == n && graph[soln[n]][soln[1]] != 0)) {
+                    if (k < n || (k == n && graph[solution[n]][solution[1]] != 0)) {
                         return;
                     }
                 }
@@ -74,8 +74,8 @@ class Hamiltonian {
         count += 1;
         // Print Solution Array
         for (int i = 1; i <= n; i++) {
-            System.out.print(soln[i] + " ");
+            System.out.print(solution[i] + " ");
         }
-        System.out.println(soln[1]);
+        System.out.println(solution[1]);
     }
 }
